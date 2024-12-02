6641e3be50ca4b23a6cc39f83ccdce2cf44b9f29
paulBlartCallMop <188939308+paulBlartCallMop@users.noreply.github.com>
2024-12-02 11:35:19 -0500

    Add gameBoard module
    The gameBoard object stores the tic tac toe board.
    It has the reset method for clearing the board,
    the move method for making a game move, The update
    method for updating the DOM with the current game
    state, checkWin for checking if a given player has
    won, checkFull for checking if the board is full.

---
+++ javascript.js
@@ -0,0 +1,69 @@
+const gameBoard = (function () {
+    let board = [];
+
+    for (let i = 0; i < 9; i++) {
+        board.push('');
+    }
+
+    let turn = 1;
+
+    const reset = () => {
+        for (let i = 0; i < 9; i++) {
+            board[i] = '';
+        }
+
+        turn = 1;
+    }
+
+    const move = (player, slot) => {
+        if (board[slot] === '') {
+            board[slot] = player;
+            return true;
+        }
+        return false;
+    }
+
+    const update = () => {
+        const tiles = document.querySelectorAll('.tile');
+        tiles.forEach((tile) => {
+            tile.textContent = board[parseInt(tile.id)]
+        });
+    }
+
+    const linesToCheck = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+
+    const checkWin = (player) => {
+        linesToCheck.forEach((line) => {
+            let lineIsWinning = true;
+            line.forEach((tile) => {
+                if (board[tile] != player) {
+                    lineIsWinning = false;
+                }
+            });
+            if (lineIsWinning) {
+                return true;
+            }
+        });
+        return false;
+    }
+
+    const checkFull = () => {
+        for (let i = 0; i < 9; i++) {
+            if (board[i] === '') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    return {reset, move, update, checkWin, checkFull}
+})();
